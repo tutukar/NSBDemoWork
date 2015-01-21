9b6bc3b04c105f861357f59e076e43b3eaecc8e8
tutukar <tutukar@users.noreply.github.com>
2015-01-21 15:18:14 -0800

    Revert "Add sample Request Response with timeout from Host"

--- AccountCreate/LoyaltyAccountSaga/Sagas/CreateAccountSaga.cs
+++ AccountCreate/LoyaltyAccountSaga/Sagas/CreateAccountSaga.cs
@@ -8,28 +8,26 @@ using Remotion.Linq.Parsing;
 
 namespace LoyaltyAccountSaga.Sagas
 {
-    public class CreateAccountSaga : Saga<AccountCreate>,
-        IAmStartedByMessages<LoyaltyAccountRequest>,
+    public class CreateAccountSaga:Saga<AccountCreate>,
+        IAmStartedByMessages<LoyaltyAccount>,
         IHandleMessages<ConfirmAccount>
     {
         protected override void ConfigureHowToFindSaga(SagaPropertyMapper<AccountCreate> mapper)
         {
             mapper.ConfigureMapping<ConfirmAccount>(s => s.EmailAddress).ToSaga(m => m.EMailAddress);
+            
         }
-        
-        public void Handle(LoyaltyAccountRequest message)
+
+        public void Handle(LoyaltyAccount message)
         {
             Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine("Recieved loyalty account request {0}", message.MessageId);
+            Console.WriteLine("Recieved new loyalty account.");
+            Console.WriteLine("Sending confirmation email.");
+            Console.WriteLine("Waiting for confirmation.......");
+            Console.ResetColor();
             Data.EMailAddress = message.LoyaltyEmail;
             //Encrypt the password using 3DES
             Data.ConfirmationCode = Guid.NewGuid().ToString();
-            Console.WriteLine("Sending confirmation email with verification code {0}", Data.ConfirmationCode);
-            Console.WriteLine("Waiting for confirmation.......");
-            
-            Console.WriteLine("Sending loyalty account response {0}", message.MessageId);
-            Console.ResetColor();
-            Bus.Reply(new LoyaltyAccountResponse { MessageId = message.MessageId });
         }
 
         public void Handle(ConfirmAccount message)
@@ -39,14 +37,16 @@ namespace LoyaltyAccountSaga.Sagas
             Console.WriteLine("Checking...");
             if (message.ConfirmationCode == Data.ConfirmationCode)
             {
+                
                 Console.WriteLine("Confirmation code verified.");
                 Console.WriteLine("Loyalty customer verified.");
                 Console.ResetColor();
                 MarkAsComplete();
             }
             //Use the account 
-
+            
             Console.WriteLine();
+            
         }
     }
 }
